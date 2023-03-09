0ddb25dda8baf94a0b920faede47a9497c961b98
c0rnP1ex <127425456+c0rnP1ex@users.noreply.github.com>
2023-03-09 23:59:10 +0800

    apis

---
+++ gpt_api.py
@@ -0,0 +1,65 @@
+import openai
+
+proxies = {
+    "http": "http://127.0.0.1:port",
+    "https": "http://127.0.0.1:port"
+}
+openai.proxy = proxies
+openai.proxy
+
+SECRET_KEY = # your api-key
+openai.api_key = SECRET_KEY
+
+ENGINE_LIST = openai.Engine.list()
+MODEL_LIST = openai.Model.list()
+MAX_TOKEN_LEN = 1024
+TIME_OUT = 3
+
+BOT_ROLE = 'assistant'
+USER_ROLE = 'user'
+initial_prompt = # your prompt
+class gpt_thread:
+    def __init__(self) -> None:
+        self.messages = []
+        self.reset_log()
+
+    def receive_message_from_api(self):
+        response = []
+        # self.messages.text.count
+        response = openai.ChatCompletion.create(
+                model='gpt-3.5-turbo-0301',
+                messages=self.messages,
+                temperature=1.0,
+                max_tokens=MAX_TOKEN_LEN,
+                top_p=0.6,
+                frequency_penalty=2.0,
+                presence_penalty=0.0,
+                timeout=TIME_OUT,
+            )
+        return response
+
+    def get_response(self, prompt):
+        self.add_user_content(prompt)
+        response = self.receive_message_from_api()
+        self.add_bot_content(response['choices'][0]['message']['content'])
+        return response
+
+    def reset_log(self):
+        self.messages = [{'role': 'system', 'content': initial_prompt}]
+
+    def add_user_content(self, content):
+        self.messages.append({'role': USER_ROLE, 'content': content})
+
+    def add_content(self, ROLE_add, content):
+        self.messages.append({'role': ROLE_add, 'content': content})
+
+    def add_bot_content(self, content):
+        self.messages.append({'role': BOT_ROLE, 'content': content})
+
+    def reset_system_content(self, content):
+        self.messages = [{'role': 'system', 'content': content}]
+
+if __name__ == '__main__':
+    gpt_thread1 = gpt_thread();
+    resp = gpt_thread1.get_response('hello')
+    print(resp['choices'][0]['message']['content'])
